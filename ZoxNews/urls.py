5e17a37c42334848845d15ce1501dd7ec14ba13e
Hayotbek2005 <hayotbekabdulazizov2005@gmail.com>
2021-11-10 12:35:06 +1200

    s

--- ZoxNews/urls.py
+++ ZoxNews/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin"),
     path('', include('main.urls', namespace='main')),
 ]
 
