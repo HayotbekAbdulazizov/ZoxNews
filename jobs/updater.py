edb2da68efc97f1d02c933d4faf609291e18dd4d
HayotbekAbdulazizov <hayotbekabdulazizov2005@gmail.com>
2021-11-26 15:34:06 +1200

    rev

--- jobs/updater.py
+++ jobs/updater.py
@@ -4,8 +4,11 @@ from .jobs import schedule_api
 
 def start():
 <<<<<<< HEAD
+<<<<<<< HEAD
 	scheduler.add_job(schedule_api, 'interval', minutes=100)
 =======
+=======
+>>>>>>> parent of bbb2baf (problem soplved with timezone)
 	scheduler = BackgroundScheduler()
 	scheduler.add_job(schedule_api, 'interval', minutes=2)
 >>>>>>> parent of bbb2baf (problem soplved with timezone)
